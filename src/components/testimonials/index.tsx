e690a8db188bfc334f4bb1c8b2caa94be29731ca
Sathish R <91242874+Sathishr150601@users.noreply.github.com>
2024-06-25 13:39:16 +0530

    Re +2

--- src/components/testimonials/index.tsx
+++ src/components/testimonials/index.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Heading from "../Heading";
 import images from "@/images";
-import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef, useLayoutEffect, useState } from "react";
+import { debounce } from "lodash";
 
 const TestimonialsHeading = {
 	title: "",
@@ -17,24 +18,31 @@ const TestimonialsData = {
 const Testimonials: React.FC = () => {
 	const { scrollY } = useScroll();
 	const sectionRef = useRef<HTMLDivElement>(null);
-	const contentRef = useRef<HTMLDivElement>(null);
-	const [start, setStart] = useState(0);
-	const [end, setEnd] = useState(0);
+	const contentRef1 = useRef<HTMLDivElement>(null);
+	const contentRef2 = useRef<HTMLDivElement>(null);
+	const [start, setStart] = useState(0); // State for start position
+	const [end1, setEnd1] = useState(0); // State for end position of first motion.div
+	const [end2, setEnd2] = useState(0); // State for end position of second motion.div
 
 	useLayoutEffect(() => {
-		const handleScroll = () => {
-			if (sectionRef.current && contentRef.current) {
+		const handleScroll = debounce(() => {
+			if (sectionRef.current && contentRef1.current && contentRef2.current) {
 				const sectionTop = sectionRef.current.getBoundingClientRect().top;
-				const contentBottom = contentRef.current.getBoundingClientRect().bottom;
+				const contentBottom1 =
+					contentRef1.current.getBoundingClientRect().bottom;
+				const contentBottom2 =
+					contentRef2.current.getBoundingClientRect().bottom;
 				const windowHeight = window.innerHeight;
 
 				const newStart = scrollY.get() + sectionTop - windowHeight / 2;
-				const newEnd = scrollY.get() + contentBottom;
+				const newEnd1 = scrollY.get() + contentBottom1;
+				const newEnd2 = scrollY.get() + contentBottom2;
 
 				setStart(newStart);
-				setEnd(newEnd);
+				setEnd1(newEnd1);
+				setEnd2(newEnd2);
 			}
-		};
+		}, 16);
 
 		handleScroll();
 		window.addEventListener("scroll", handleScroll);
@@ -43,7 +51,8 @@ const Testimonials: React.FC = () => {
 		};
 	}, [scrollY]);
 
-	const y1 = useTransform(scrollY, [start, end], [0, 500]);
+	const y1 = useTransform(scrollY, [start, end1], [-100, 600]);
+	const y2 = useTransform(scrollY, [start, end2], [0, 1000]);
 
 	return (
 		<>
@@ -68,11 +77,23 @@ const Testimonials: React.FC = () => {
 					/>
 				</div>
 
+				{/* First motion.div */}
 				<motion.div
 					style={{ y: y1 }}
-					className="w-full h-[100vh] lg:h-[200vh] relative z-10 flex justify-center items-center"
-					id="ContentRef"
-					ref={contentRef}
+					className="w-full h-[100vh] lg:h-[200vh] relative z-10 flex justify-center items-center lg:hidden"
+					ref={contentRef1}
+				>
+					<h3 className="text-[11vw] leading-snug text-center text-white p-[10%]">
+						The Results that Astounds our Clients are an attestation to our
+						success.
+					</h3>
+				</motion.div>
+
+				{/* Second motion.div */}
+				<motion.div
+					style={{ y: y2 }}
+					className="w-full h-[100vh] lg:h-[200vh] relative z-10 flex justify-center items-center max-lg:hidden"
+					ref={contentRef2}
 				>
 					<h3 className="text-[11vw] leading-snug text-center text-white p-[10%]">
 						The Results that Astounds our Clients are an attestation to our
